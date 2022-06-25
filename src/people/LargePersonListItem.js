d0b0c40c4750481d511172e6343bde124b5dfaaf
Bakary Sylla <syllabakary2002@gmail.com>
2022-06-25 01:27:34 -0700

    Different List type with basic style

--- src/people/LargePersonListItem.js
+++ src/people/LargePersonListItem.js
@@ -3,10 +3,10 @@ export const LargePersonListItem = ({person}) => {
 
     return (
         <>
-        <h3>{name}</h3> 
-        <p>Age: {age} years</p> 
-        <p>Hair Color: {hairColor}</p> 
-        <h3>hobbies</h3>
+        <h3 style={{backgroundColor: "gold"}}>{name}</h3> 
+        <p style={{backgroundColor: "gold"}}>Age: {age} years</p> 
+        <p style={{backgroundColor: "gold"}}>Hair Color: {hairColor}</p> 
+        <h3 style={{backgroundColor: "gold"}}>hobbies</h3>
         <ul>
             {hobbies.map(hobby => <li key={hobby}>{hobby}</li>)}
         </ul> 
